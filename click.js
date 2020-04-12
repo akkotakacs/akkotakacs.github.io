f5febf77c9f646424ffb7b1811c2b452aa49f281
akkotakacs <63321824+akkotakacs@users.noreply.github.com>
2020-04-12 20:40:06 +0200

    add counter.html

--- click.js
+++ click.js
@@ -1,5 +1,20 @@
 console.log('Nice to see a button :)')
-// click.html
+
+// -- counter.html --
+let counter = 0
+$('h3').text(counter)
+$('#increase').click(() => {
+  counter++
+  $('h3').text(counter)
+});
+
+$('#decrease').click(() => {
+  counter--
+  $('h3').text(counter)
+});
+
+
+// -- click.html --
 
 $('#first').click(() => {
   console.log('Yeah, you clicked me.')
@@ -15,9 +30,22 @@ $('#second').click(() => {
 
 $('#third').click(() => {
   let szin = $("#myInput").val();
-  $('button').css('background', szin)
+  if (confirm('You chose the following button background color: ' + szin)) {
+    $('button').css('background', szin)
+    console.log(szin)
+  } else {
+    console.log(szin + ' cancelled.')
+  }
 });
 
 // only the first input is read..
 // let szin = $("#myInput").val();
         // alert(szin);
+// alert( "You have " + $(".overdue").size() + " books overdue." );
+
+// -- Confirm Box operation --
+// if (confirm("Press a button!")) {
+//   txt = "You pressed OK!";
+// } else {
+//   txt = "You pressed Cancel!";
+// }
